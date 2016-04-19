61d97beffaadf05dd86e2bb0b1397dfd79f64006
Andy Bangs <andrew.yuta@gmail.com>
2016-04-19 12:03:39 -0700

    setup db

---
+++ app_server/models/games.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+var gameSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  description: String,
+  user: String,
+  url: String,
+  created: Date,
+  alphabetical: {type: Boolean, "default": false},
+  suddenDeath: {type: Boolean, "default": false},
+  studyMode: {type: Boolean, "default": true},
+  timerSeconds: {type: Number, "default": 120, min: 0},
+  data: [[String]]
+});
+
+mongoose.model('Game', gameSchema);
